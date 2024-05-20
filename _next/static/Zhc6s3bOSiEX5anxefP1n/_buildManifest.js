8c2c6b83bfaeaeeebab4092af15217e83869b626
Andy Edwards <jedwards@fastmail.com>
2024-05-19 22:04:31 -0500

    Updates

---
+++ _next/static/Zhc6s3bOSiEX5anxefP1n/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-25b5715e9f3b7a61.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
