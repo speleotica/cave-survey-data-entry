e297819455e290c92e05b78c0638bdbe9be74f9d
Andy Edwards <jedwards@fastmail.com>
2024-05-19 22:09:55 -0500

    chore: fix gh pages build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: process.env.NEXT_BASE_PATH || undefined,
+  output: "export",
+};
 
 export default nextConfig;
